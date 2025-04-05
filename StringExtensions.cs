007676ae3c3d3d43227015d594d47065b1c62433
ritikaraut12 <rraut1@my.centennialcollege.ca>
2025-04-05 11:16:37 -0400

    Initial commit

---
+++ StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+            return char.IsUpper(input[0]);
+        }
+    }
+}
